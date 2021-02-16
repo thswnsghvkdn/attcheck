a964d466b9063f4c6f9a005088f4f23018db202a
thswnsghvkdn <ths3630802@naver.com>
2021-02-16 19:28:55 +0900

    demo

--- client/src/App.js
+++ client/src/App.js
@@ -59,7 +59,7 @@ render = () => {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          AAA <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
@@ -67,7 +67,7 @@ render = () => {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          ABCDE
         </a>
       </header>
 
--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -1,3 +1,4 @@
+/*
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
 
@@ -19,3 +20,4 @@ app.use(
         })
     );
 };
+*/
